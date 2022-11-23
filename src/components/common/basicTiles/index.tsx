da66f0131aa56673f524b2b8bc3e84a197a5dc1c
vaibhav <vaibhav.j@techsophy.com>
2022-11-23 19:34:23 +0530

    first commit

---
+++ src/components/common/basicTiles/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import COLORS from '../../../theme/colors.json';
+import './index.scss';
+import { useTranslation } from 'react-i18next';
+
+const useStyles = makeStyles(() => ({
+    tileBox: {
+        backgroundColor: COLORS.APP_THEME_COLOR,
+        color: COLORS.WHITE,
+    },
+}));
+
+const BasicTiles = ({ data }) => {
+    const { t } = useTranslation();
+    const { title } = data;
+    const classes = useStyles();
+    return (
+        <div className={classes.tileBox + ' tileBox'}>
+            <div className={'tileValue'}>
+                value
+            </div>
+            <div className="tileTitle">{t(title)}</div>
+        </div>
+    );
+};
+
+export default BasicTiles;
---
+++ src/constants/endpoints.ts
@@ -0,0 +1,5 @@
+export const TEMPLATE_UPLOAD = '/dms/v1/documents?documentTypeId=887760084437041153';
+export const TEMPLATE_DOWNLOAD = '/dms/v1/documents';
+export const GENERATE_OTP = '';
+export const VERIFY_OTP = '';
+export const KEYCLOAK_TOKEN = 'auth/realms/techsophy-platform/protocol/openid-connect/token';
